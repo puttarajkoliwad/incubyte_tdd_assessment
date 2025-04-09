dbca276f0e0eefdb1e4992a887230ff1e3ec7c83
Puttaraj Koliwad <puttarajkoliwad@gmail.com>
2025-04-10 02:38:03 +0530

    complete add function

--- app.ts
+++ app.ts
@@ -2,4 +2,13 @@ import { add } from "./src/calculator";
 
 const ans1 = add('1,2');
 
+const ans2 = add(`1
+  3`);
+
 console.log(ans1)
+console.log(ans2)
+console.log(add("1,2,3,4"))
+console.log(add("1\n2,3"))
+console.log(add("//;\n1;2"))
+console.log(add("1,-2"))
+// console.log(add("1,-2,3,-5"))
